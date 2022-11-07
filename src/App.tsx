b8aa0a10a3e49fae41132138d7a2c69fd825c62c
xieyu567 <xieyu345800@sina.com>
2022-11-07 08:38:27 +0800

    add hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import AnimalExample from './components/AnimalExample'
+import AnimalExampleWithApiStatus from './components/AnimalExampleWithApiStates'
 import './App.css'
 
 function App() {
   return (
     <div className='App mx-auto max-w-6xl text-center my-8'>
       <h1 className='font-semibold text-2xl'>React - Test</h1>
-      <AnimalExample />
+      <AnimalExampleWithApiStatus />
     </div>
   )
 }
